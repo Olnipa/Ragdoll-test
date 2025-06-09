633588c04108cc012a1daae8976b3fdaad557c1f
OLN <oleghack@gmail.com>
2025-06-09 18:11:24 +0700

    Add debugger

---
+++ Assets/Source/Scripts/ForDebugOnly.cs
@@ -0,0 +1,22 @@
+﻿using Source.Scripts.Services.ScenesManagement;
+using UnityEngine;
+using Zenject;
+
+namespace Source.Scripts
+{
+  public class ForDebugOnly : MonoBehaviour
+  {
+#if UNITY_EDITOR
+    [Inject]
+    private IGameSceneService _sceneService;
+
+    private AsyncOperation _asyncOperation;
+
+    private void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Space)) 
+        _sceneService.ReloadGame();
+    }
+#endif
+  }
+}
