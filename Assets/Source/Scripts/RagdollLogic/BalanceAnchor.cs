424a386b52325a38c147bb664cbb7d977e4ea8ae
OLN <oleghack@gmail.com>
2025-06-10 12:17:56 +0700

    Add balance anchor

---
+++ Assets/Source/Scripts/RagdollLogic/BalanceAnchor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Source.Scripts.RagdollLogic
+{
+    public class BalanceAnchor : MonoBehaviour
+    {
+        [SerializeField] private float _breakForce = 500f;
+        [SerializeField] private Rigidbody _mainBody;
+    
+        private FixedJoint _supportJoint;
+    
+        void Start()
+        {
+            GameObject balanceAnchor = new GameObject("BalanceAnchor");
+            balanceAnchor.transform.position = transform.position;
+            Rigidbody anchorRb = balanceAnchor.AddComponent<Rigidbody>();
+            anchorRb.isKinematic = true;
+        
+            _supportJoint = _mainBody.gameObject.AddComponent<FixedJoint>();
+            _supportJoint.connectedBody = anchorRb;
+            _supportJoint.breakForce = _breakForce;
+            _supportJoint.breakTorque = _breakForce;
+        }
+    }
+}
